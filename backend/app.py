5ae9abb9e15a34fd31f4054b8c2c4f6f58699079
Drages <richard001122@126.com>
2026-01-29 22:38:02 +0800

    update

--- backend/app.py
+++ backend/app.py
@@ -19,4 +19,4 @@ app.register_blueprint(lineup_bp, url_prefix="/api/lineup")
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    app.run(debug=False, port=5000, host='0.0.0.0')
