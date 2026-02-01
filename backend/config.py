22bbd3767d0a47b491b21bdfb1ca2db8ca8df77a
Drages <richard001122@126.com>
2026-02-01 15:48:09 +0800

    update

--- backend/config.py
+++ backend/config.py
@@ -8,7 +8,7 @@ db = SQLAlchemy()
 
 def init_db(app: Flask):
     db_path = os.path.join(
-        os.path.dirname(os.path.dirname(__file__)), "database", "ScoutsLens.db"
+        os.path.dirname(os.path.dirname(__file__)), "database", "scoutslens.db"
     )
     app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
